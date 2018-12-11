72a792759cd6357058bff8d361cc32e4ba57913b
marty-p <37085112+marty-p@users.noreply.github.com>
2018-12-11 16:40:07 +0100

    Add files via upload

--- Assets/Scripts/AnimationEvent.cs
+++ Assets/Scripts/AnimationEvent.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class AnimationEvent : MonoBehaviour {
 
+    private Animator animator;
+
 	// Use this for initialization
 	void Start () {
-		
+        animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -16,6 +18,7 @@ public class AnimationEvent : MonoBehaviour {
 
     public void Event()
     {
+        animator.SetBool("Bounce", true);
         Debug.Log("Ho chiamato del codice tramite animazione");
     }
 }
