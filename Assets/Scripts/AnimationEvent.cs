0611ca0ac1396c47d733e3c9044e02b9bbf582a4
marty-p <37085112+marty-p@users.noreply.github.com>
2018-12-11 16:19:26 +0100

    Add files via upload

---
+++ Assets/Scripts/AnimationEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEvent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Event()
+    {
+        Debug.Log("Ho chiamato del codice tramite animazione");
+    }
+}
